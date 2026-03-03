d12c91d041bf29913a6fef76056fe129af48df02
yablochnyi <artem.yablochnyi@yandex.ru>
2026-03-03 15:58:25 +0100

    tag

--- resources/views/marketing.blade.php
+++ resources/views/marketing.blade.php
@@ -79,6 +79,15 @@
     @if ($appleTouchRel)
         <link rel="apple-touch-icon" href="{{ asset($appleTouchRel) }}">
     @endif
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N67PPKX0S8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-N67PPKX0S8');
+    </script>
 </head>
 <body>
 <nav class="nav">
